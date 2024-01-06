8b6b448810d524da313dae5e8992121e023ab123
riblanc <richard.blc25@gmail.com>
2024-01-06 23:50:59 +0100

    ADD mmio_console printf method:
    Adding:
    - A BufferWriter type, (a simple std.io.Writer, with the Buffer.write method as callback)
    - A writer field on the BufferN struct
    - An init_writer method to init the writer according to the current Buffer instance
    - A printf method using the writer to format a string and pass it to the Buffer.write method

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,6 +1,7 @@
 const mmio_console = @import("./mmio_console.zig");
 
 fn init_console(c: *mmio_console.Buffer) void {
+    c.init_writer();
     c.set_font_color(mmio_console.Color.white);
     c.set_view_bottom();
 }
@@ -9,6 +10,6 @@ var console = mmio_console.Buffer{};
 
 export fn kernel_main() void {
 	init_console(&console);
-    console.putstr("hello, world");
+    console.printf("hello, {}", .{42});
     console.view();
 }
