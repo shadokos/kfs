8ec89f99495709591f89728ec3a8a58f91f169eb
joachim <jgiron@student.42.fr>
2024-01-07 12:46:56 +0100

    ADD mmio_console vt100-like escape codes support:
    Add the following terminal capabilities:
    - set attribute
    - move cursor
    - goto cursor
    - scroll
    - clear line
    - clear screen

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,15 +1,15 @@
 const mmio_console = @import("./mmio_console.zig");
 
-fn init_console(c: *mmio_console.Buffer) void {
+fn init_console(c: *mmio_console.BufferN(100)) void {
     c.init_writer();
     c.set_font_color(mmio_console.Color.white);
     c.set_view_bottom();
 }
 
-var console = mmio_console.Buffer{};
+var console = mmio_console.BufferN(100){};
 
 export fn kernel_main() void {
 	init_console(&console);
-    console.printf("hello, {}", .{42});
+    console.printf("hello, \x1b[32m{}\x1b[38m", .{42});
     console.view();
 }
