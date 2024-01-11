904aedce7af59717da716dc9cd20952e29187e82
riblanc <richard.blc25@gmail.com>
2024-01-12 00:51:26 +0100

    REBASE ri/keyboard with jo/add_mmio_console

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,14 +1,6 @@
 const tty = @import("./tty/tty.zig");
 const keyboard = @import("./keyboard.zig");
 
-fn init_console(c: *tty.Tty) void {
-    c.init_writer();
-    c.set_font_color(tty.Color.white);
-    c.set_view_bottom();
-}
-
-var console = tty.Tty {};
-
 export fn kernel_main() void {
 	const console = &tty.tty_array[tty.current_tty];
     try console.writer().print("hello, \x1b[32m{d}\x1b[37m", .{42});
