3bc92390bb55941ca24f3b91aafe4d84393f765e
joachim <jgiron@student.42.fr>
2024-01-11 15:25:26 +0100

    ADD ft.io.Reader

--- src/ft/io.zig
+++ src/ft/io.zig
@@ -1,3 +1,5 @@
 const ft = @import("ft.zig");
 
 pub const Writer = @import("io/writer.zig").Writer;
+
+pub const Reader = @import("io/writer.zig").Reader;
---
+++ src/ft/io/reader.zig
@@ -0,0 +1,30 @@
+const ft = @import("../ft.zig");
+
+pub fn Reader(
+	comptime Context: type,
+	comptime ReadError: type,
+	comptime readFn: fn (Context, []u8) ReadError!usize
+) type {
+	return struct {
+		context: Context,
+
+		const Self = @This();
+
+		const Error = ReadError;
+
+		pub fn read(self: Self, bytes: []u8) Error!usize {
+			return readFn(self.context, bytes);
+		}
+
+		pub fn readAll(self: Self, bytes: []u8) Error!void {
+			var offset: usize = 0;
+			while (offset != bytes.len) {
+				offset += try self.read(bytes[offset..]);
+			}
+		}
+
+		pub fn readByte(self: Self, byte: u8) Error!void {
+			_ = try callback(self.context, &byte);
+		}
+	};
+}
