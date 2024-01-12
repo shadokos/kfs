fcd3dd4781148d28aa23acf1ebf75b3187b2e69f
joachim <jgiron@student.42.fr>
2024-01-12 20:10:17 +0100

    FIX kernel_main:
    reset color to white

--- src/kernel.zig
+++ src/kernel.zig
@@ -3,6 +3,6 @@ const keyboard = @import("./tty/keyboard.zig");
 
 export fn kernel_main() void {
 	const console = &tty.tty_array[tty.current_tty];
-    try console.writer().print("hello, \x1b[32m{d}\x1b[38m", .{42});
+    try console.writer().print("hello, \x1b[32m{d}\x1b[37m", .{42});
     console.view();
 }
