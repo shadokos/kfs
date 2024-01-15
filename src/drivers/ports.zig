a48ab0a30bc6737b775eaf7caf122ac31080a35e
riblanc <richard.blc25@gmail.com>
2024-01-15 12:03:31 +0100

    ADD more function to manipulate io ports:
    Adding more function to manipulate i/o ports including:
    - inw / outw to read / write 2 bytes.
    - inl / outl to read / write 4 bytes

--- src/drivers/ports.zig
+++ src/drivers/ports.zig
@@ -10,3 +10,19 @@ pub fn inb(port: Ports) u8 {
 pub fn outb(port: Ports, data: u8) void{
 	asm volatile("outb %[data], %[port]" : : [data] "{al}" (data), [port] "{dx}" (port));
 }
+
+pub fn inw(port: Ports) u16 {
+	return asm volatile("inw %[port], %[result]" : [result] "={ax}" (-> u16) : [port] "N{dx}" (port));
+}
+
+pub fn outw(port: Ports, data: u16) void{
+	asm volatile("outw %[data], %[port]" : : [data] "{ax}" (data), [port] "{dx}" (port));
+}
+
+pub fn inl(port: Ports) u32 {
+	return asm volatile("inl %[port], %[result]" : [result] "={eax}" (-> u32) : [port] "N{dx}" (port));
+}
+
+pub fn outl(port: Ports, data: u32) void{
+	asm volatile("outl %[data], %[port]" : : [data] "{eax}" (data), [port] "{dx}" (port));
+}
