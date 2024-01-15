88a0bab0979cdfef344e38a60c9c504249e3ee07
riblanc <59930230+riblanc@users.noreply.github.com>
2024-01-15 15:55:16 +0100

    Apply suggestions from code review

--- src/drivers/ports.zig
+++ src/drivers/ports.zig
@@ -7,7 +7,7 @@ pub fn inb(port: Ports) u8 {
     return asm volatile("inb %[port], %[result]" : [result] "={al}" (-> u8) : [port] "N{dx}" (port));
 }
 
-pub fn outb(port: Ports, data: u8) void{
+pub fn outb(port: Ports, data: u8) void {
 	asm volatile("outb %[data], %[port]" : : [data] "{al}" (data), [port] "{dx}" (port));
 }
 
@@ -15,7 +15,7 @@ pub fn inw(port: Ports) u16 {
 	return asm volatile("inw %[port], %[result]" : [result] "={ax}" (-> u16) : [port] "N{dx}" (port));
 }
 
-pub fn outw(port: Ports, data: u16) void{
+pub fn outw(port: Ports, data: u16) void {
 	asm volatile("outw %[data], %[port]" : : [data] "{ax}" (data), [port] "{dx}" (port));
 }
 
@@ -23,6 +23,6 @@ pub fn inl(port: Ports) u32 {
 	return asm volatile("inl %[port], %[result]" : [result] "={eax}" (-> u32) : [port] "N{dx}" (port));
 }
 
-pub fn outl(port: Ports, data: u32) void{
+pub fn outl(port: Ports, data: u32) void {
 	asm volatile("outl %[data], %[port]" : : [data] "{eax}" (data), [port] "{dx}" (port));
 }
--- src/kernel.zig
+++ src/kernel.zig
@@ -3,12 +3,6 @@ const keyboard = @import("./tty/keyboard.zig");
 
 export fn kernel_main() void {
 	const console = &tty.tty_array[tty.current_tty];
-    try console.writer().print("hello, \x1b[32m{d}\x1b[24B\x1b[24A\x1b[37m\n", .{42});
+    try console.writer().print("hello, \x1b[32m{d}\x1b[37m\n", .{42});
     console.view();
-
-   	while (true) {
-   		keyboard.simulate_keyboard_interrupt();
-   		_ = keyboard.kb_read();
-   		console.view();
-   	}
 }
