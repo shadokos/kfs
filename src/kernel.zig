2aa2b572084c1e3a9bd65aa6fc482200027f9faa
joachim <jgiron@student.42.fr>
2024-01-16 18:56:16 +0100

    REWORK tty:
    - add a Reader object and a reader function to Tty
    - better handling of multiple terminals
    - better interface for tty with the addition of several "shortcut" functions

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,8 +1,6 @@
-const tty = @import("./tty/tty.zig");
 const keyboard = @import("./tty/keyboard.zig");
+const printk = @import("./tty/tty.zig").printk;
 
 export fn kernel_main() void {
-	const console = &tty.tty_array[tty.current_tty];
-    try console.writer().print("hello, \x1b[32m{d}\x1b[37m\n", .{42});
-    console.view();
+    printk("hello, \x1b[32m{d}\x1b[37m\n", .{42});
 }
