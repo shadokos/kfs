cc702cf6144b9baebfdfa03fe5317501fcf6345d
riblanc <59930230+riblanc@users.noreply.github.com>
2024-01-22 18:47:04 +0100

    REWORK src/tty/keyboard.zig:
    Remove useless stuff
    
    Co-authored-by: Joachim Giron <59100853+jgiron42@users.noreply.github.com>

--- src/tty/keyboard.zig
+++ src/tty/keyboard.zig
@@ -82,7 +82,7 @@ fn make_break(scancode: u16) ?u16 {
 			keyState.num_down = make;
 		},
 		keymap.AF1...keymap.AF10 => if (keyState.ctrl and !make) {
-			_ = tty.set_tty(@intCast(c - keymap.AF1)) catch unreachable;
+			tty.set_tty(@intCast(c - keymap.AF1)) catch unreachable;
 		},
 		else => if (make and c != 0)
 				return c,
