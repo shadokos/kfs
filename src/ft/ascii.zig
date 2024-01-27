6a727eacde649e4c30f83a8efb9229200e098a99
riblanc <richard.blc25@gmail.com>
2024-01-27 03:52:41 +0100

    ADD isWhitespace function:
    Adding a function "isWhitespace" in ft.ascii.

--- src/ft/ascii.zig
+++ src/ft/ascii.zig
@@ -1,5 +1,13 @@
 const ft = @import("ft.zig");
 
+pub const whitespace = [_]u8{ ' ', '\t', '\n', '\r', 11, 12 };
+
 pub fn isDigit(c: u8) bool {
 	return c >= '0' and c <= '9';
+}
+
+pub fn isWhitespace(c: u8) bool {
+	return for (whitespace) |w| {
+		if (c == w) break true;
+	} else false;
 }
