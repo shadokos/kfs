46d9a10285899e7e354f360e2411a3fc83febe16
joachim <jgiron@student.42.fr>
2024-02-02 12:31:38 +0100

    REWORK boot:
    rework bootloader interface:
    - _entry is now only calling the init function with eax and ebx as arguments
    - add a check for eax in init

--- src/boot.zig
+++ src/boot.zig
@@ -11,25 +11,30 @@ export var stack_bottom : [*]u8 = @as([*]u8, @ptrCast(&stack)) + @sizeOf(@TypeOf
 
 export var multiboot_header : multiboot.header_type align(4) linksection(".multiboot") = multiboot.get_header();
 
+pub var multiboot_info : *multiboot.info_header = undefined;
+
 export fn _entry() callconv(.Naked) noreturn {
 	asm volatile(
 		\\ mov stack_bottom, %esp
 		\\ movl %esp, %ebp
-		\\ movl %ebx, multiboot_info
+		\\ push %ebx
+		\\ push %eax
 		\\ call init
 	);
 	while (true) {}
 }
 
-pub export var multiboot_info : * volatile multiboot.info_header = undefined;
+export fn init(eax : u32, ebx : *multiboot.info_header) void {
+	if (eax == multiboot2_h.MULTIBOOT2_BOOTLOADER_MAGIC) {
+		multiboot_info = ebx;
+	} else @panic("No multiboot2 magic number");
+
+	kernel_main();
+}
 
 pub fn panic(msg: []const u8, _: ?*builtin.StackTrace, _: ?usize) noreturn {
 	const tty = @import("tty/tty.zig");
 
 	tty.printk("panic: {s}\n", .{msg});
 	while (true) {}
-}
-
-export fn init() void {
-	kernel_main();
 }
