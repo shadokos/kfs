c820d126b417c070dac9037b5cc627524397bff0
joachim <jgiron@student.42.fr>
2024-02-27 17:22:54 +0100

    ADD permissions in mapping:
    User space is owned by User.
    Kernel space is owned by supervisor.
    
    read only section of the kernel are set to read only (for extra safety)

--- src/memory.zig
+++ src/memory.zig
@@ -2,7 +2,6 @@ const boot = @import("boot.zig");
 const ft = @import("ft/ft.zig");
 const tty = @import("./tty/tty.zig");
 const printk = @import("./tty/tty.zig").printk;
-// const BuddyAllocator = @import("memory/buddy_allocator.zig").BuddyAllocator;
 const PageFrameAllocator = @import("memory/page_frame_allocator.zig").PageFrameAllocator;
 const paging = @import("memory/paging.zig");
 const multiboot = @import("multiboot.zig");
@@ -14,27 +13,19 @@ pub const VirtualPageAllocatorType = VirtualPageAllocator(PageFrameAllocator);
 
 pub var virtualPageAllocator : VirtualPageAllocatorType = .{};
 
-// const VirtualAddressesAllocator = @import("virtual_addresses_allocator.zig").VirtualAddressesAllocator;
-//
-// const VirtualAddressesAllocatorType = VirtualAddressesAllocator(VirtualPageAllocatorType);
-//
-// pub var kernel_addresses_allocator : VirtualAddressesAllocatorType = .{};
-
-
 pub var pageFrameAllocator : PageFrameAllocator = .{};
 
 var total_space : u64 = undefined;
 
-// pub const Mapper = mapping(@TypeOf(pageFrameAllocator));
-//
-// pub var mapper : Mapper = .{};
-
-extern var stack_bottom : [*]u8;
-
 pub fn map_kernel() void {
+
+	// create space for all the tables of the kernel space
 	const tables : * align(4096) [256][paging.page_table_size]paging.page_table_entry = @ptrCast((virtualPageAllocator.alloc_pages_opt(256, .{.type = .KernelSpace}) catch @panic("todo")));
+	// at the end of this functions the page tables are only unmapped but not freed
 	defer virtualPageAllocator.unmap(@ptrCast(tables), 256 * paging.page_size);
-	for (0..254) |i| {
+
+	// init all the page tables of the kernelspace
+	for (0..(paging.kernel_virtual_space_size >> 22)) |i| {
 		if (paging.page_table_table_ptr[768 + i].present) {
 			@memcpy(tables[i][0..], mapping.get_table_ptr(@intCast(768 + i))[0..]);
 			paging.page_dir_ptr[768 + i].address_fragment = @intCast(mapping.get_physical_ptr(@ptrCast(&tables[i])) >> 12);
@@ -45,13 +36,32 @@ pub fn map_kernel() void {
 		}
 	}
 
+	// set kernelspace to supervisor only mapping
+	for (0..256) |i| {
+		paging.page_table_table_ptr[768 + i].owner = .Supervisor;
+	}
+
+	// set read only kernel sections to non writable mapping for extra safety
+	if (multiboot.get_tag(multiboot2_h.MULTIBOOT_TAG_TYPE_ELF_SECTIONS)) |t| {
+		var iter = multiboot.section_hdr_it{.base = t};
+		while (iter.next()) |e| {
+			if (e.sh_addr >= paging.low_half) {
+				if (!e.sh_flags.SHF_WRITE) {
+					virtualPageAllocator.mapper.set_rights(@ptrFromInt(e.sh_addr), ft.math.divCeil(u32, e.sh_size, paging.page_size) catch unreachable, false);
+				}
+			}
+		}
+	}
+
+	// free the spaces taken by the early page tables
 	for (&@import("trampoline.zig").page_tables) |*table| {
 		pageFrameAllocator.free_pages(@intFromPtr(table));
 		const mapped = virtualPageAllocator.map_anywhere(@intFromPtr(table), paging.page_size, .KernelSpace) catch @panic("todo");
-		@memset(@as(*[4096]u8, @ptrCast(@alignCast(mapped))), 0);
+		@memset(@as(paging.VirtualPagePtr, @ptrCast(@alignCast(mapped))), 0);
 		virtualPageAllocator.unmap(mapped, paging.page_size);
 	}
 
+	// shrink the kernel executable space to its actual size
 	const kernel_aligned_begin = ft.mem.alignForward(usize, @intFromPtr(boot.kernel_end) + paging.low_half, paging.page_size);
 	const kernel_aligned_end = ft.mem.alignForward(usize, @import("trampoline.zig").kernel_size + paging.low_half, paging.page_size);
 	virtualPageAllocator.unmap(@ptrFromInt(kernel_aligned_begin), kernel_aligned_end - kernel_aligned_begin);
@@ -87,9 +97,6 @@ fn check_mem_availability() void {
 			while (area_begin < area_end) : (area_begin += page_size) {
 				if (area_begin < @intFromPtr(boot.kernel_end)) // area is before kernel end
 					continue;
-				// if ((area_begin > boot.linearAllocator.lower_bound() and area_begin < boot.linearAllocator.upper_bound()) or
-				// 	((area_begin + page_size) > boot.linearAllocator.lower_bound() and (area_begin + page_size) < boot.linearAllocator.upper_bound())) // area overlap space allocated by the linear_allocator
-				// 	continue;
 				pageFrameAllocator.free_pages(area_begin);
 			}
 		}
