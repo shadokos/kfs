6aff469d5b9c613e0159cbab26fb5cf555cd996f
riblanc <richard.blc25@gmail.com>
2024-03-12 01:52:12 +0100

    UPDATE ft log:
    Finishing basic implementation for ft log.
    Adding a red screen of death.

---
+++ src/screen_of_death.zig
@@ -0,0 +1,106 @@
+const tty = @import("tty/tty.zig");
+const utils = @import("shell/utils.zig");
+
+const logo =
+"    +   .                                   \n" ++
+"    :  :                                    \n" ++
+"     . ;                                    \n" ++
+" -..   +$                                   \n" ++
+"    `+_ .$                                  \n" ++
+"      _ +;$;  $$+                           \n" ++
+"   +`    +$$$$$$$$$$$$x.                    \n" ++
+" x`         X$$$$$$$$$$$$x                  \n" ++
+";          $$$+$$$$$$$$$$$$.                \n" ++
+":         +$$$+;$$$$$$$$$$$$$$$$x.          \n" ++
+"        .o;-----.$$$$$$$$$$$$$$$$$$x.       \n" ++
+"       $$ $$;-'`   ;X$$$$$$$$$$$$$$$$$+     \n" ++
+"      $$ $X $$$$$;;$x $$.$   :$$$$$$$$$$    \n" ++
+"      . $$ $$  $ $ .$  $        $$$$$$$$$   \n" ++
+"     .  : $$  $; $  $ $           $$;$$$$x  \n" ++
+"      ..  :   $x $  $$ $x          .  $$$$  \n" ++
+"        : :    + +   $$  X            .$$$+ \n" ++
+"               .  ;    x              $$$$  \n" ++
+"                   ;                .$$$$X  \n" ++
+"       $&&&  &&&  &&&&&&  ;&&&&;   $$$$$$.  \n" ++
+"       .&&x &&.   x&&:   &&&     :$$$$$$$   \n" ++
+"       .&&x&&     x&&&&  x&&&&$  $$$$$$     \n" ++
+"       .&&x&&&    x&&:      &&&&;           \n" ++
+"       .&&x &&&   x&&:   x   .&&;           \n" ++
+"       $&&&  &&&; &&&&   ;&&&&&             \n";
+
+
+
+
+const letter_o =
+" \x01\x01\x01 \n" ++
+"\x01   \x01\n" ++
+"\x01   \x01\n" ++
+"\x01   \x01\n" ++
+" \x01\x01\x01 \n";
+
+const letter_h =
+"\x01   \x01\n" ++
+"\x01   \x01\n" ++
+"\x01\x01\x01\x01\x01\n" ++
+"\x01   \x01\n" ++
+"\x01   \x01\n";
+
+const letter_n =
+"\x01   \x01\n" ++
+"\x01\x01  \x01\n" ++
+"\x01 \x01 \x01\n" ++
+"\x01  \x01\x01\n" ++
+"\x01   \x01\n";
+
+const letter_exclamation =
+"  \x01  \n" ++
+"  \x01  \n" ++
+"  \x01  \n" ++
+"     \n" ++
+"  \x01  \n";
+
+pub fn print_ascii_art(
+	image: []const u8,
+	coord: struct {x: u16, y: u16},
+	colors: struct {bg: []const u8, fg: []const u8}
+) void {
+	var _coord = coord;
+	tty.printk("\x1b[2m\x1b[37m{s}{s}", .{colors.bg, colors.fg});
+	for (image) |c| switch (c) {
+		' ' => _coord.x += 1,
+		'\n' => {
+			_coord.x = coord.x;
+			_coord.y += 1;
+		},
+		else => {
+			tty.printk(
+				"\x1b[{d};{d}H{s}",
+				.{_coord.y, _coord.x, [1]u8{if (c != '\x01') c else ' '}},
+			);
+			_coord.x += 1;
+		},
+	};
+}
+
+pub fn screen_of_death(comptime format: []const u8, args: anytype) void {
+	const dim	   = "\x1b[2m";
+	const bg_red   = "\x1b[41m";
+	const bg_black = "\x1b[0m";
+	const fg_white = "\x1b[37m";
+	const fg_black = "\x1b[30m" ++ dim;
+
+	tty.printk(bg_red ++ "{s}", .{(" " ** (tty.width*tty.height))});
+	tty.get_tty().scroll(1);
+	print_ascii_art(logo, .{.x = 37, .y = 0}, .{.bg = bg_red, .fg = fg_white});
+	print_ascii_art(letter_o, .{.x = 3, .y = 2}, .{.bg = bg_black, .fg = fg_white});
+	print_ascii_art(letter_h, .{.x = 10, .y = 2}, .{.bg = bg_black, .fg = fg_white});
+	print_ascii_art(letter_n, .{.x = 21, .y = 2}, .{.bg = bg_black, .fg = fg_white});
+	print_ascii_art(letter_o, .{.x = 28, .y = 2}, .{.bg = bg_black, .fg = fg_white});
+	print_ascii_art(letter_exclamation, .{.x = 33, .y = 2}, .{.bg = bg_black, .fg = fg_white});
+	tty.printk("\x1b[9;3H" ++ fg_black ++ bg_red ++ "{s}", .{
+		"######## KERNEL PANIC! #########"
+	});
+	tty.printk("\x1b[11;3H" ++ utils.reset ++ fg_white ++ bg_red, .{});
+	tty.printk(format, args);
+	while (true) {}
+}
