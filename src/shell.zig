e5f00196452d45369de0b5c2f2c55c9160a8a763
riblanc <richard.blc25@gmail.com>
2024-03-17 19:23:29 +0100

    UPDATE Shell:
    Defining a more generic Shell structure, to be able to declare a Shell with a given builtins file and custom reader / writer.
    Builtins now take the shell context as first parameter.

--- src/shell.zig
+++ src/shell.zig
@@ -11,62 +11,73 @@ var status_code: usize = 0;
 
 pub const CmdError = error{ CommandNotFound, InvalidNumberOfArguments, InvalidParameter, OtherError };
 
-fn exec_cmd(args: [][]u8) CmdError!void {
-    // Search for a builtin command by iterating over those defined in shell/builtins.zig
-    inline for (@typeInfo(Builtins).Struct.decls) |decl| {
-        if (ft.mem.eql(u8, decl.name, args[0])) {
-            return @field(Builtins, decl.name)(args);
+pub fn Shell(comptime _builtins: anytype) type {
+    return struct {
+        const builtins = _builtins;
+        const Self = @This();
+        var err: bool = false;
+
+        reader: ft.io.AnyReader = undefined,
+        writer: tty.Tty.Writer = undefined,
+
+        pub fn init(_reader: ft.io.AnyReader, _writer: tty.Tty.Writer) Self {
+            const ret = Self{
+                .reader = _reader,
+                .writer = _writer,
+            };
+            return ret;
         }
-    }
-    return CmdError.CommandNotFound;
-}
 
-pub fn shell() u8 {
-    var reader = tty.get_reader();
-    tty.get_tty().config.c_lflag.ECHOCTL = true;
-
-    var err: bool = false;
-
-    while (true) {
-        utils.print_prompt(err);
-
-        // Read a line from the tty
-        const slice = reader.readUntilDelimiterAlloc(allocator, '\n', 4096) catch |e| {
-            if (e == error.StreamTooLong) {
-                utils.print_error("Line is too long", .{});
-                _ = reader.skipUntilDelimiterOrEof('\n') catch {};
+        fn exec_cmd(self: *Self, args: [][]u8) CmdError!void {
+            // Search for a builtin command by iterating over those defined in shell/builtins.zig
+            inline for (@typeInfo(builtins).Struct.decls) |decl| {
+                if (ft.mem.eql(u8, decl.name, args[0])) {
+                    return @field(builtins, decl.name)(self, args);
+                }
             }
-            err = true;
-            continue;
-        };
-        defer allocator.free(slice);
+            return CmdError.CommandNotFound;
+        }
 
-        // Tokenize the line
-        const args = token.tokenize(slice) catch |e| {
-            switch (e) {
-                token.TokenizerError.InvalidQuote => utils.print_error("invalid quotes", .{}),
-                token.TokenizerError.MaxTokensReached => utils.print_error(
-                    "too many tokens (max: {d})",
-                    .{token.max_tokens},
-                ),
-            }
-            err = true;
-            continue;
-        };
-        if (args.len == 0) continue;
+        pub fn routine(self: *Self) void {
+            utils.print_prompt(err);
 
-        utils.ensure_newline();
+            // Read a line from the reader
+            var slice = self.reader.readUntilDelimiterAlloc(allocator, '\n', 4096) catch |e| {
+                if (e == error.StreamTooLong) {
+                    utils.print_error("Line is too long", .{});
+                    _ = self.reader.skipUntilDelimiterOrEof('\n') catch {};
+                }
+                err = true;
+                return;
+            };
+            defer allocator.free(slice);
 
-        err = false;
-        exec_cmd(args) catch |e| {
-            err = true;
-            switch (e) {
-                CmdError.CommandNotFound => utils.print_error("{s}: command not found", .{args[0]}),
-                CmdError.InvalidNumberOfArguments => utils.print_error("Invalid number of arguments", .{}),
-                CmdError.InvalidParameter => utils.print_error("Invalid parameter", .{}),
-                CmdError.OtherError => {}, // specific errors are handled by the command itself
-            }
-        };
-    }
-    return 0;
+            // Tokenize the line
+            const args = token.tokenize(slice) catch |e| {
+                switch (e) {
+                    token.TokenizerError.InvalidQuote => utils.print_error("invalid quotes", .{}),
+                    token.TokenizerError.MaxTokensReached => utils.print_error(
+                        "too many tokens (max: {d})",
+                        .{token.max_tokens},
+                    ),
+                }
+                err = true;
+                return;
+            };
+            if (args.len == 0) return;
+
+            utils.ensure_newline();
+
+            err = false;
+            self.exec_cmd(args) catch |e| {
+                err = true;
+                switch (e) {
+                    CmdError.CommandNotFound => utils.print_error("{s}: command not found", .{args[0]}),
+                    CmdError.InvalidNumberOfArguments => utils.print_error("Invalid number of arguments", .{}),
+                    CmdError.InvalidParameter => utils.print_error("Invalid parameter", .{}),
+                    CmdError.OtherError => {}, // specific errors are handled by the command itself
+                }
+            };
+        }
+    };
 }
