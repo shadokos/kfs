f4b43fcb8cc874d9da31468b86b83867461f7dd8
riblanc <richard.blc25@gmail.com>
2024-03-23 03:15:54 +0100

    ADD workflow for in-kernel unit tests:
    Adding a workflow to run in-kernel unit tests.
    It introduces a new Makefile rule "ci", and a python script "ci_test.py" listing commands to send

--- src/shell/ci/builtins.zig
+++ src/shell/ci/builtins.zig
@@ -1,9 +1,38 @@
-const ft = @import("../../ft/ft.zig");
 const Packet = @import("packet.zig").Packet;
 
 pub fn cmd_test(shell: anytype, _: anytype) void {
     var packet = Packet(void).init(shell);
-    ft.fmt.format(shell.writer, "This is a test... 0x{x:0>4}\n", .{0x42}) catch {};
+    shell.writer.print("This is a test... 0x{x:0>4}\n", .{0x42}) catch {};
+    packet.type = .Success;
+    packet.send();
+}
+
+pub fn ultimate_answer(shell: anytype, _: anytype) void {
+    var packet = Packet(void).init(shell);
+    shell.writer.print("Calculating Ultimate Answer..\n", .{0x42}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Calculating Trajectories\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Overlaying Grid onto Bezier Curves\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Patching Conics\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Biding Time\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Untangling Space Tape\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("Recalibrating Density Scalen\n", .{}) catch {};
+    for (0..250_000_000) |_| asm volatile ("nop");
+    shell.writer.print("The Ultimate Answer is ...\n", .{}) catch {};
+    for (0..750_000_000) |_| asm volatile ("nop");
+    shell.writer.print("{d}\n", .{42}) catch {};
+    packet.type = .Success;
+    packet.send();
+}
+
+pub fn shadok(shell: anytype, _: anytype) void {
+    var packet = Packet(void).init(shell);
+    shell.writer.print("Ga Bu Zo Meu (ѷ)\n", .{}) catch {};
     packet.type = .Success;
     packet.send();
 }
