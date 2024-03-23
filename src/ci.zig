a07e9aaf3594ad579dc8674e9b26a3f5fba3fa07
riblanc <richard.blc25@gmail.com>
2024-03-23 02:27:19 +0100

    UPDATE CI main:
    Using the new shell features (like hooks) to cleaning up the ci.main method.
    Introducing a new type "Packet" to send data to host through serial port (with kind of json-like serialization)

--- src/ci.zig
+++ src/ci.zig
@@ -1,28 +1,12 @@
-const Shell = @import("shell/Shell.zig").Shell(@import("shell/ci/builtins.zig"));
-const Serial = @import("drivers/serial_port/serial.zig");
-const pic = @import("drivers/pic/pic.zig");
-const ft = @import("ft/ft.zig");
-const cpu = @import("cpu.zig");
-const tty = @import("tty/tty.zig");
-
-var com_port_1: Serial = Serial.init(.com_port_1);
-var shell = Shell.init(com_port_1.get_reader().any(), com_port_1.get_writer().any(), .{
-    .colors = false,
-}, .{});
-
-fn handler() callconv(.Interrupt) void {
-    pic.ack();
-}
-
-pub var packet = {};
+const ci_shell = @import("shell/ci/shell.zig");
 
 pub fn main() void {
-    const interrupts = @import("interrupts.zig");
-
-    com_port_1.activate() catch return ft.log.err("Failed to enable COM1", .{});
-
-    interrupts.set_trap_gate(pic.IRQS.COM1, interrupts.Handler{ .noerr = &handler });
-    pic.enable_irq(pic.IRQS.COM1);
+    var shell = ci_shell.Shell.init(
+        ci_shell.com_port_1.get_reader().any(),
+        ci_shell.com_port_1.get_writer().any(),
+        .{ .colors = false },
+        .{ .on_init = &ci_shell.on_init, .on_error = &ci_shell.on_error },
+    );
 
     while (true) shell.process_line();
 }
