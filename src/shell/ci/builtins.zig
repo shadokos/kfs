4a45f8314cab15c7b1c71e4f1db3557d5c1b845d
riblanc <richard.blc25@gmail.com>
2024-03-23 02:27:19 +0100

    UPDATE CI main:
    Using the new shell features (like hooks) to cleaning up the ci.main method.
    Introducing a new type "Packet" to send data to host through serial port (with kind of json-like serialization)

--- src/shell/ci/builtins.zig
+++ src/shell/ci/builtins.zig
@@ -1,7 +1,11 @@
 const ft = @import("../../ft/ft.zig");
+const Packet = @import("packet.zig").Packet;
 
 pub fn cmd_test(shell: anytype, _: anytype) void {
+    var packet = Packet(void).init(shell);
     ft.fmt.format(shell.writer, "This is a test... 0x{x:0>4}\n", .{0x42}) catch {};
+    packet.type = .Success;
+    packet.send();
 }
 
 pub fn quit(_: anytype, _: anytype) void {
