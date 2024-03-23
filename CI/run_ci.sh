5ced94f30d16fbbc9854e8f78e71a3f2b0064a33
riblanc <richard.blc25@gmail.com>
2024-03-23 22:02:19 +0100

    REWORK ci structure:
    Create a new CI directory containing iso folder, Dockerfile and scripts related to CI

---
+++ CI/run_ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd -- $(dirname $0)/..
+working_dir=$(pwd)
+
+docker build CI -t qemu
+docker run --rm --name qemu -p 4444:4444 -w /build -v ./:/build:rw -d \
+  qemu -cdrom kfs.iso -nographic -serial tcp::4444,server,nowait && sleep 5
+python3 CI/ci_commands.py
+ret=$?
+docker stop qemu 1>/dev/null 2>/dev/null
+exit $ret
