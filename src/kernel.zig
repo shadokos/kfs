591233efa371f389f45e3efa0256ec9e334a40c1
joachim <jgiron@student.42.fr>
2024-04-23 12:03:52 +0200

    UPDATE tasks base

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,42 +1,43 @@
 const DefaultShell = @import("shell/default/shell.zig");
 const tty = @import("./tty/tty.zig");
 
-var t: task.TaskUnion = undefined;
-var t1: task.TaskUnion = undefined;
-var t2: task.TaskUnion = undefined;
+const task = @import("task/task.zig");
+const task_set = @import("task/task_set.zig");
+const scheduler = @import("task/scheduler.zig");
 
 fn task1() void {
     const logger = @import("ft/ft.zig").log.scoped(.task1);
     logger.info("invoked", .{});
-    task.switch_to_task(&t1, &t);
+    scheduler.schedule();
     while (true) {
         logger.info("tic", .{});
         for (0..100_000_000) |_| asm volatile ("nop");
-        task.switch_to_task(&t1, &t2);
+        scheduler.schedule();
     }
 }
 
 fn task2() void {
     const logger = @import("ft/ft.zig").log.scoped(.task2);
     logger.info("invoked", .{});
-    task.switch_to_task(&t2, &t);
+    scheduler.schedule();
     while (true) {
         logger.info("tac", .{});
         for (0..100_000_000) |_| asm volatile ("nop");
-        task.switch_to_task(&t2, &t1);
+        scheduler.schedule();
     }
 }
-
-const task = @import("task/process/process.zig");
 pub fn main() void {
     task.TaskUnion.init_cache() catch @panic("Failed to initialized kernel_task cache");
-    t = task.TaskUnion.init(0, &t, &t);
-    task.switch_to_task(&t, &t);
 
-    task.clone(task1, @ptrFromInt(@as(u32, @intFromPtr(&t1.stack)) + t1.stack.len));
-    task.clone(task2, @ptrFromInt(@as(u32, @intFromPtr(&t2.stack)) + t2.stack.len));
+    const kernel = task_set.create_task() catch @panic("c'est  la  panique 2");
+    scheduler.add_task(kernel);
 
-    task.switch_to_task(&t, &t1);
+    _ = task.spawn(task1) catch @panic("c'est  la  panique 3");
+    _ = task.spawn(task2) catch @panic("c'est  la  panique 4");
+
+    while (true) {
+        scheduler.schedule();
+    }
 
     var shell = DefaultShell.Shell.init(tty.get_reader(), tty.get_writer(), .{}, .{
         .on_init = DefaultShell.on_init,
