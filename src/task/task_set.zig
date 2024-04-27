06432453cdde284c71ad26b74c86b7ceab5565ef
joachim <jgiron@student.42.fr>
2024-04-27 13:14:47 +0200

    UPDATE tasks:
    - improve task architecture
    - add wait

--- src/task/task_set.zig
+++ src/task/task_set.zig
@@ -1,6 +1,7 @@
 const ft = @import("../ft/ft.zig");
 const task = @import("task.zig");
 const TaskDescriptor = task.TaskDescriptor;
+const scheduler = @import("scheduler.zig");
 
 const NTASK = 100; // todo
 
@@ -11,8 +12,17 @@ var count: usize = 0;
 pub fn create_task() !*TaskDescriptor {
     const pid = next_pid() orelse return error.TooMuchProcesses;
     const new_task = try task.TaskUnion.cache.allocator().create(task.TaskUnion);
-    new_task.task = .{ .pid = pid };
-    list[pid] = &new_task.task;
+    const parent = scheduler.get_current_task();
+    if (pid == 0) {
+        new_task.task = .{ .pid = pid, .pgid = pid, .parent = null, .state = .Running };
+        scheduler.init(&new_task.task);
+    } else {
+        new_task.task = .{ .pid = pid, .pgid = parent.pgid, .parent = parent, .state = .Running };
+        new_task.task.next_sibling = parent.childs;
+        parent.childs = &new_task.task;
+        scheduler.add_task(&new_task.task);
+    }
+    list[@intCast(pid)] = &new_task.task;
     count += 1;
     return &new_task.task;
 }
@@ -21,7 +31,7 @@ fn next_pid() ?TaskDescriptor.Pid {
     if (count == NTASK) {
         return null;
     }
-    while (list[head]) |_| {
+    while (list[@intCast(head)]) |_| {
         head += 1;
         if (head == NTASK) {
             head = 0;
@@ -31,12 +41,17 @@ fn next_pid() ?TaskDescriptor.Pid {
 }
 
 pub fn get_task_descriptor(pid: TaskDescriptor.Pid) ?*TaskDescriptor {
-    return list[pid];
+    if (pid < 0) return null;
+    return list[@intCast(pid)];
 }
 
-pub fn remove_task(pid: TaskDescriptor.Pid) !void {
-    const descriptor = list[pid] orelse return error.NoSuchTask; // todo error
-    list[pid] = null;
+pub fn destroy_task(pid: TaskDescriptor.Pid) !void {
+    if (pid < 0) return error.NoSuchTask;
+    const index: u32 = @intCast(pid);
+    const descriptor = list[index] orelse return error.NoSuchTask; // todo error
+    scheduler.remove_task(descriptor);
+    descriptor.deinit();
+    list[index] = null;
     count -= 1;
     task.TaskUnion.cache.allocator().destroy(descriptor);
 }
