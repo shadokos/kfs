4d3cc8f36a2bbf9d5e372ce04a577ad004e83579
joachim <jgiron@student.42.fr>
2024-12-02 19:59:14 +0100

    bip bip boup boup

--- src/drivers/pit/pit.zig
+++ src/drivers/pit/pit.zig
@@ -143,9 +143,10 @@ pub fn init_channel(comptime channel: SelectChannel, frequency: u32) void {
     pit_logger.debug("{s} initialized", .{@tagName(channel)});
 }
 
-pub fn pit_handler(_: interrupts.InterruptFrame) callconv(.C) void {
+pub fn pit_handler(_: *interrupts.InterruptFrame) callconv(.C) void {
     ch0_ticks +%= 1;
     pic.ack(.Timer);
+    @import("../../task/scheduler.zig").schedule();
 }
 
 pub fn sleep_n_ticks(ticks: u64) void {
