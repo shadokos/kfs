cc9d04e9dfaf3751f5169fcfc62752d412ae532e
joachim <jgiron@student.42.fr>
2025-01-07 16:05:01 +0100

    REFACTOR TaskDescriptor:
    put stack directly in TaskDescriptor, get rid of TaskUnion

---
+++ src/syscall_table.zig
@@ -0,0 +1,5 @@
+pub const write = @import("syscall/write.zig");
+pub const poc_raise = @import("syscall/poc_raise.zig");
+pub const sleep = @import("syscall/sleep.zig");
+pub const reboot = @import("syscall/reboot.zig");
+pub const sigreturn = @import("syscall/sigreturn.zig");
