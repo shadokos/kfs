069287bf0a7d2aa7a49a4823c7f52652b382ac80
riblanc <richard.blc25@gmail.com>
2025-01-14 01:30:14 +0100

    ADD scheduler's lock logic:
    This commit a basic lock logic to prevents race conditions during task switching.
    It relies on a lock_count variables.
    
    The scheduler's locking logic is pretty simple:
    - during a lock:
      1. a "cli" is performed
      2. the lock_count is incremented
    - during an unlock:
      1. if lock_count is already set to 0, then a panic occurs.
      2. lock_count is decremented
      3. if lock_count == 0 a "sti" is performed.
    
    The scheduler lock allows us to protect some critical section during task switching / scheduling etc and to keep track the number of locks for each task.

--- src/task/scheduler.zig
+++ src/task/scheduler.zig
@@ -2,32 +2,48 @@ const ft = @import("ft");
 const task = @import("task.zig");
 const task_set = @import("task_set.zig");
 
+pub var lock_count: u32 = 0;
 var current_task: *task.TaskDescriptor = undefined;
+var initialized: bool = false;
+
+pub fn init(new_task: *task.TaskDescriptor) void {
+    @This().lock();
+    initialized = true;
+    current_task = new_task;
+    current_task.next = new_task;
+    current_task.prev = new_task;
+    @This().unlock();
+}
+
+pub inline fn lock() void {
+    @import("../cpu.zig").disable_interrupts();
+    lock_count += 1;
+}
+
+pub inline fn unlock() void {
+    lock_count -= if (lock_count > 0) 1 else @panic("Trying to unlock unlocked scheduler");
+    if (lock_count == 0) @import("../cpu.zig").enable_interrupts();
+}
 
 pub fn add_task(new_task: *task.TaskDescriptor) void {
+    @This().lock();
     new_task.next = current_task.next;
     new_task.prev = current_task;
     new_task.next.prev = new_task;
     new_task.prev.next = new_task;
-}
-
-var initialized: bool = false;
-
-pub fn init(new_task: *task.TaskDescriptor) void {
-    initialized = true;
-    current_task = new_task;
-    new_task.next = new_task;
-    new_task.prev = new_task;
+    @This().unlock();
 }
 
 pub fn remove_task(t: *task.TaskDescriptor) void {
+    @This().lock();
     t.prev.next = t.next;
     t.next.prev = t.prev;
+    @This().unlock();
 }
 
 pub fn schedule() void {
     if (!initialized) return;
-    @import("../cpu.zig").disable_interrupts();
+    @This().lock();
     const prev = current_task;
     current_task = current_task.next;
     while (current_task.state != .Running and current_task != prev) {
@@ -37,15 +53,19 @@ pub fn schedule() void {
         @panic("no task running");
     }
     task.switch_to_task(prev, current_task);
-    @import("../cpu.zig").enable_interrupts();
+    @This().unlock();
 }
 
 pub fn checkpoint() void {
+    @This().lock();
     task.switch_to_task(current_task, current_task);
+    @This().unlock();
 }
 
 pub inline fn set_current_task(new_task: *task.TaskDescriptor) void {
+    @This().lock();
     current_task = new_task;
+    @This().unlock();
 }
 
 pub fn get_current_task() *task.TaskDescriptor {
