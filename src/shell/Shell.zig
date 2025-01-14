84bff81b84f6b38643333168b918065237e106fc
joachim <jgiron@student.42.fr>
2025-01-14 18:03:36 +0100

    UPDATE shell:
    add support for detaching commands

--- src/shell/Shell.zig
+++ src/shell/Shell.zig
@@ -2,6 +2,8 @@ const ft = @import("ft");
 const token = @import("token.zig");
 const colors = @import("colors");
 const allocator = @import("../memory.zig").smallAlloc.allocator();
+const task = @import("../task/task.zig");
+const task_set = @import("../task/task_set.zig");
 
 pub const CmdError = error{ CommandNotFound, InvalidNumberOfArguments, InvalidParameter, OtherError };
 pub const Config = struct {
@@ -27,7 +29,12 @@ pub fn Shell(comptime _builtins: anytype) type {
 
         const ExecutionContext = struct {
             args: [][]u8,
+            detach: bool = false,
             err: ?anyerror,
+        };
+        const Job = struct {
+            pid: task.TaskDescriptor.Pid,
+            command_line: []u8,
         };
 
         config: Config = Config{},
@@ -38,6 +45,7 @@ pub fn Shell(comptime _builtins: anytype) type {
         reader: ft.io.AnyReader = undefined,
         writer: ft.io.AnyWriter = undefined,
         hooks: Hooks = Hooks{},
+        jobs: ft.ArrayList(Job) = undefined,
 
         pub fn init(
             reader: ft.io.AnyReader,
@@ -52,6 +60,7 @@ pub fn Shell(comptime _builtins: anytype) type {
             },
         ) Self {
             var ret = Self{
+                .jobs = ft.ArrayList(Job).init(allocator),
                 .reader = reader,
                 .writer = writer,
                 .config = config,
@@ -65,17 +74,71 @@ pub fn Shell(comptime _builtins: anytype) type {
             return ret;
         }
 
-        fn exec_cmd(self: *Self, args: [][]u8) CmdError!void {
+        fn cmd_arguments(function: anytype) type {
+            return struct {
+                shell: *Self,
+                args: [][]u8,
+
+                fn start_cmd(data: anytype) u8 {
+                    const self: *cmd_arguments(function) = data;
+                    const ret: u8 = if (function(self.shell, self.args)) |_| 0 else |e| @truncate(
+                        @intFromError(e),
+                    ); //todo
+                    allocator.destroy(self);
+                    return ret;
+                }
+            };
+        }
+
+        fn exec_detached(self: *Self, comptime function: anytype, args: [][]u8) ?task.TaskDescriptor.Pid {
+            const CmdStruct = cmd_arguments(function);
+            const arg_struct: *CmdStruct = allocator.create(CmdStruct) catch {
+                self.print_error("Out of space", .{});
+                return null;
+            };
+            arg_struct.* = .{
+                .shell = self,
+                .args = args,
+            };
+            const descriptor = task_set.create_task() catch @panic("c'est  la  panique 5"); // todo
+            descriptor.spawn(&CmdStruct.start_cmd, arg_struct) catch @panic("c'est  la  panique 5");
+            return descriptor.pid;
+        }
+
+        fn exec_cmd(self: *Self, args: [][]u8, detach: bool) CmdError!?task.TaskDescriptor.Pid {
             // Search for a builtin command by iterating over those defined in shell/builtins.zig
             inline for (@typeInfo(builtins).@"struct".decls) |decl| {
                 if (ft.mem.eql(u8, decl.name, args[0])) {
-                    return @field(builtins, decl.name)(self, args);
+                    if (detach) {
+                        return self.exec_detached(@field(builtins, decl.name), args);
+                    } else {
+                        try @field(builtins, decl.name)(self, args);
+                        return null;
+                    }
                 }
             }
             return CmdError.CommandNotFound;
         }
 
+        fn wait_childs(self: *Self) void {
+            const wait = @import("../task/wait.zig");
+            var status: wait.Status = undefined;
+            var i: u32 = 0;
+            while (i < self.jobs.slice.len) {
+                const job = self.jobs.slice[i];
+                if (wait.wait(job.pid, .SELF, &status, .{ .WNOHANG = true }) catch @panic("todo")) |_| {
+                    self.print("[{}] done {s}\n", .{ job.pid, job.command_line });
+                    allocator.destroy(job.command_line);
+                    _ = self.jobs.swapRemove(i);
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
         pub fn process_line(self: *Self) void {
+            self.wait_childs();
+
             self.execution_context.args = &.{};
             if (self.hooks.pre_prompt) |hook| hook(self);
 
@@ -86,7 +149,6 @@ pub fn Shell(comptime _builtins: anytype) type {
                 if (self.hooks.on_error) |hook| hook(self);
                 return;
             };
-            defer allocator.free(slice);
 
             // Tokenize the line
             self.execution_context.args = token.tokenize(slice) catch |e| {
@@ -94,16 +156,28 @@ pub fn Shell(comptime _builtins: anytype) type {
                 if (self.hooks.on_error) |hook| hook(self);
                 return;
             };
+            if (self.execution_context.args.len > 0 and ft.mem.eql(u8, self.execution_context.args[0], "&")) {
+                self.execution_context.detach = true;
+                self.execution_context.args = self.execution_context.args[1..];
+            } else {
+                self.execution_context.detach = false;
+            }
             if (self.execution_context.args.len == 0) return;
 
             if (self.hooks.pre_cmd) |hook| hook(self);
 
             self.execution_context.err = null;
-            self.exec_cmd(self.execution_context.args) catch |e| {
+            if (self.exec_cmd(self.execution_context.args, self.execution_context.detach) catch |e| {
                 self.execution_context.err = e;
                 if (self.hooks.on_error) |hook| hook(self);
+                allocator.free(slice);
                 return;
-            };
+            }) |pid| {
+                self.jobs.append(.{ .pid = pid, .command_line = slice }) catch @panic("todo");
+                self.print("[{}]\n", .{pid});
+            } else {
+                allocator.free(slice);
+            }
             if (self.hooks.post_cmd) |hook| hook(self);
         }
 
