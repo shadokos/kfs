ddb4ddce174f07beef2196ba3e846aebe53649e1
joachim <jgiron@student.42.fr>
2025-02-08 15:27:47 +0100

    ADD syscalls:
    add the following syscalls:
    - kill
    - getpid
    - getppid

---
+++ src/syscall/kill.zig
@@ -0,0 +1,25 @@
+pub const Id = 3;
+const scheduler = @import("../task/scheduler.zig");
+const signal = @import("../task/signal.zig");
+const task = @import("../task/task.zig");
+const task_set = @import("../task/task_set.zig");
+const Errno = @import("../errno.zig").Errno;
+
+// todo: invalid id
+pub fn do(pid: task.TaskDescriptor.Pid, id: signal.Id) !void {
+    if (pid > 0) {
+        const descriptor = task_set.get_task_descriptor(pid) orelse return Errno.ESRCH;
+        // todo permisssion
+        descriptor.signalManager.queue_signal(.{
+            .si_signo = @intFromEnum(id),
+            // todo set more fields of siginfo
+        });
+    } else if (pid == 0) {
+        // todo: process group
+    } else if (pid == -1) {
+        // todo: every process for which the calling process has  per-
+        // mission to send signals, except for process 1
+    } else { // pid < -1
+        // todo: process group with id -id
+    }
+}
--- src/syscall/poc_raise.zig
+++
@@ -1,7 +0,0 @@
-pub const Id = 3;
-const scheduler = @import("../task/scheduler.zig");
-const signal = @import("../task/signal.zig");
-
-pub fn do(id: signal.Id) !void {
-    signal.kill(scheduler.get_current_task().pid, id) catch @panic("todo");
-}
