db37bb12477b76032b262f64cc01e2a203032828
joachim <jgiron@student.42.fr>
2025-02-08 14:43:06 +0100

    REWORK signal:
    Add the ucontext struct to manipulate the user context.
    Make the signal delivery interface rely on it.
    Refactor the interrupt interface to make the interrupt frame constant (since it will be loaded to and from the task structure at the beginning and the end of each interrupt).

---
+++ src/syscall/signal.zig
@@ -0,0 +1,8 @@
+const scheduler = @import("../task/scheduler.zig");
+const signal = @import("../task/signal.zig");
+
+pub const Id = 8;
+
+pub fn do(id: signal.Id, handler: signal.Handler) !void {
+    scheduler.get_current_task().signalManager.change_action(id, handler) catch @panic("todo");
+}
