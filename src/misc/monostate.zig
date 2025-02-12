bdfe1543c00118c6c6b4a8a2d091fdcbc3f42b1b
joachim <jgiron@student.42.fr>
2025-02-12 14:20:58 +0100

    REFACTOR tss:
    use monostate in tss

--- src/misc/monostate.zig
+++ src/misc/monostate.zig
@@ -1,5 +1,6 @@
+// A type that can only be assigned one value
 pub fn Monostate(comptime T: type, comptime value: T) type {
-    return packed struct {
+    return packed struct(T) {
         _: enum(T) {
             _State = value,
         } = ._State,
