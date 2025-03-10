8e08b20a0a4a6e3b8d6b24b887a5261456cebf7f
riblanc <richard.blc25@gmail.com>
2025-03-10 16:45:25 +0100

    REWORK ready_queue:
    Declare the ready_queue.Node.data as a bool instead of a ?*usize.

--- src/task/ready_queue.zig
+++ src/task/ready_queue.zig
@@ -2,12 +2,12 @@ const ft = @import("ft");
 const scheduler = @import("scheduler.zig");
 const TaskDescriptor = @import("task.zig").TaskDescriptor;
 
-// Queue.Node.Data is a pointer to the ready_queue (here defined as ?*usize to avoid circular dependencies).
+// Queue.Node.Data is a bool.
 // It is used to check efficiently if a task is already in the ready_queue when adding it, or even to avoid
 // removing it twice (which would break the DoublyLinkedList len)
-// If we wanna save some memor, we could set the data type to void (@SizeOf(void) == 0), but then we would have
+// If we wanna save some memory, we could set the data type to void (as @SizeOf(void) == 0), but then we would have
 // to do some linear search to perform our checks.
-const Queue = ft.DoublyLinkedList(?*usize);
+const Queue = ft.DoublyLinkedList(bool);
 
 pub const Node = Queue.Node;
 
@@ -17,10 +17,10 @@ pub fn push(new_node: *TaskDescriptor) void {
     scheduler.lock();
     defer scheduler.unlock();
 
-    if (new_node.rq_node.data == null)
+    if (new_node.rq_node.data == false)
         ready_queue.append(&new_node.rq_node);
 
-    new_node.rq_node.data = @ptrCast(&ready_queue);
+    new_node.rq_node.data = true;
     new_node.state = .Ready;
 }
 
@@ -29,7 +29,7 @@ pub fn pop() ?*Queue.Node {
     defer scheduler.unlock();
 
     const node = ready_queue.popFirst();
-    if (node) |n| n.data = null;
+    if (node) |n| n.data = false;
     return node;
 }
 
@@ -37,10 +37,10 @@ pub fn remove(t: *TaskDescriptor) void {
     scheduler.lock();
     defer scheduler.unlock();
 
-    if (t.rq_node.data == null) return;
+    if (t.rq_node.data == false) return;
 
     ready_queue.remove(&t.rq_node);
-    t.rq_node.data = null;
+    t.rq_node.data = false;
 
     // Should we set the task to a different state here ???
     // Not sure, as this method is called during an exit, right after setting it to .Zombie state.
