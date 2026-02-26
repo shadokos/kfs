5b8066095b8897984cd2502a63092b5c5897ea6f
riblanc <richard.blc25@gmail.com>
2026-02-27 00:28:45 +0100

    ADD: Serial TTY driver and TTY chardev registration
    New drivers/tty/serial_tty.zig:
    - Inlined UART 16550 I/O (probe, activate, read, write)
    - TtyDriver backend for serial ports (COM1-COM4)
    - IRQ-driven receive via drain_uart()
    - set_termios for baud rate / line parameter changes..
    
    New drivers/tty/tty_cdev.zig:
    - Register /dev/tty0..4 (VGA consoles, major 4, minor 0+)
    - Register /dev/ttyS0..3 (serial ports, major 4, minor 64+)
    
    Deleted drivers/serial_port/serial.zig (replaced by serial_tty).
    
    Rewired CI shell and logger to use serial_tty instead of the
    deleted Serial driver.

--- src/shell/ci/shell.zig
+++ src/shell/ci/shell.zig
@@ -1,26 +1,13 @@
-const Serial = @import("../../drivers/serial_port/serial.zig");
-const pic = @import("../../drivers/pic/pic.zig");
 const std = @import("std");
-const Packet = @import("packet.zig").Packet;
-const interrupts = @import("../../interrupts.zig");
 
 pub const Shell = @import("../Shell.zig").Shell(@import("builtins.zig"));
-pub var com_port_1: Serial = Serial.init(.com_port_1);
-
-fn pic_handler(_: interrupts.InterruptFrame) void {
-    pic.ack(.COM1);
-}
 
 pub fn on_init(shell: *Shell) void {
-    com_port_1.activate() catch return std.log.err("Failed to enable COM1", .{});
-    std.log.info("COM1 enabled", .{});
-
-    interrupts.set_trap_gate(pic.IRQ.COM1, interrupts.Handler.create(&pic_handler, false));
-    pic.enable_irq(pic.IRQ.COM1);
-    _ = shell.writer.write("COM1 IRQ enabled\n") catch {};
+    _ = shell.writer.write("CI shell ready on ttyS0\n") catch {};
 }
 
 pub fn on_error(shell: *Shell) void {
+    const Packet = @import("packet.zig").Packet;
     const err = shell.execution_context.err orelse unreachable;
 
     var packet = Packet(void).init(shell.writer);
